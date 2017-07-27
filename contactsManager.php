7545eb5e51af995c6127ec1b7172d654b85f8951
Douglass Hooker <DouglassHooker@gmail.com>
2017-07-27 14:42:52 -0500

    made FILENAME a constant

--- contactsManager.php
+++ contactsManager.php
@@ -1,7 +1,10 @@
 <?php
 require 'lib.php';
-//======================================================================
+define("FILENAME", 'contacts.txt');
 // Using "readDoc() and writeDoc()" from lib.php
+//======================================================================
+//						Title Bar
+//======================================================================
 function titleBar(){
 	printf("|=====================================================================================|\n");
 	printf("|   ,---.          |              |             ,-.-.                                 |\n");
@@ -12,19 +15,31 @@ function titleBar(){
 	printf("|=====================================================================================|\n");
 }
 
-function viewContacts($doc){
-  $contents = readDoc($doc);
-  $contents = explode("\n",$contents);
-  foreach ($contents as $key => $person) {
-    $person = explode('|',$person);
-    $person[1] = substr($person[1],0,3) . "-" . substr($person[1],3,3) . "-" . substr($person[1],6,4);
-    $personInfo['name'] = $person[0];
-    $personInfo['number'] = $person[1];
-    $contents[$key] = $personInfo;
+
+//===============================================================================================
+//                                        makeContactArray
+//===============================================================================================
+function makeContactArray($doc){
+  	$contactsArray = readDoc($doc);
+	$contactsArray = explode("\n",$contactsArray);
+	foreach ($contactsArray as $key => $person) {
+	    $person = explode('|',$person);
+	    $person[1] = substr($person[1],0,3) . "-" . substr($person[1],3,3) . "-" . substr($person[1],6,4);
+	    $personInfo['name'] = $person[0];
+	    $personInfo['number'] = $person[1];
+	    $contactsArray[$key] = $personInfo;
     }
+    return $contactsArray;
+  }
+
+//===============================================================================================
+//                                        viewContacts
+//===============================================================================================
+function viewContacts($contactsArray){
+  
   
   $names = [];
-  foreach ($contents as $key => $value) {
+  foreach ($contactsArray as $key => $value) {
   		array_push($names, $value['name']);
   }
   $names = array_map('strlen',$names);
@@ -35,13 +50,15 @@ function viewContacts($doc){
   echo ' | ';
   echo "NUMBER\n";
 
-  foreach ($contents as $key => $person) {
+  foreach ($contactsArray as $key => $person) {
     echo str_pad($person['name'], $longest) . " | " . str_pad($person['number'], $longest) . PHP_EOL;
   }
-  return options($doc);
+  return options(FILENAME);
 }
 
-
+//===============================================================================================
+//                                        promptUser
+//===============================================================================================
 function promptUser(){
   do{
     fwrite(STDOUT, "Please Select an Action by Number: ");
@@ -50,45 +67,71 @@ function promptUser(){
   return $response;
   echo "\r";
 }
+
+//===============================================================================================
+//                                        options
+//===============================================================================================
 function options($doc){
   fwrite(STDOUT, "\n\nActions:\n 1. View Contacts\n 2. Add Contact\n 3. Delete Contact\n 4. Search Contacts\n 5. Exit\n");
   switch (promptUser()) {
     case 1:
-    echo "You have selected Contacts\n";
-    clearScreen();
-    titleBar();
-    viewContacts($doc);
-    break;
+    	//View Contacts
+	    clearScreen();
+	    titleBar();
+	    viewContacts(makeContactArray($doc));
+	    break;
     case 2:
-    echo "You have selected Add Contact\n";
-    break;
+    	//Add
+	    echo "You have selected Add Contact\n";
+	    break;
     case 3:
-    echo "You have selected Delete Contact\n";
-    break;
+    	//Delete
+	    echo "You have selected Delete Contact\n";
+	    break;
     case 4:
-    echo "You have selected Search Contacts\n";
-    break;
+    	//Search
+	    echo "You have selected Search Contacts\n";
+	    break;
     case 5:
-    echo "You have selected Exit\n";
-    break;
-    case null:
-    useSameLine();
-    echo "Please enter a valid response!\n";
-    break;
+    	//Exit
+	    echo "You have selected Exit\n";
+	    break;
+    case "\n":
+	    useSameLine();
+	    echo "Please enter a valid response!\n";
+	    break;
     default:
-    useSameLine();
-    echo "Please enter a valid response: ";
-    break;
+	    useSameLine();
+	    echo "Please enter a valid response: ";
+	    break;
   }
 }
+
+//===============================================================================================
+//                                        Search Function
+//===============================================================================================
+
+// function searchContacts() {
+
+// }
+
+
+
+
+
+
+
+
+
+
 //===============================================================================================
 //                                        Actual Program
 //===============================================================================================
-$doc = 'contacts.txt';
+
 
 clearScreen();
 titleBar();
-options($doc);
+options(FILENAME);
 
 
 
