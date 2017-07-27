31a24781dff25485e2c089c2dd323c2729a5d9a0
Douglass Hooker <DouglassHooker@gmail.com>
2017-07-27 11:33:05 -0500

    Added Switch case that calls functions. Clear line functionality added.

--- contactsManager.php
+++ contactsManager.php
@@ -1,1 +1,60 @@
 <?php
+	require 'lib.php';
+//======================================================================
+// Using "readDoc() and writeDoc()" from lib.php 
+//======================================================================
+
+	$doc = 'contacts.txt';
+	$handle = fopen('contacts.txt', 'a');
+
+	echo `clear`;
+
+
+	printf("|=====================================================================================|\n");
+	printf("|   ,---.          |              |             ,-.-.                                 |\n");
+	printf("|   |    ,---.,---.|--- ,---.,---.|--- ,---.    | | |,---.,---.,---.,---.,---.,---.   |\n");
+	printf("|   |    |   ||   ||    ,---||    |    `---.    | | |,---||   |,---||   ||---'|       |\n");
+	printf("|   `---'`---'`   '`---'`---^`---'`---'`---'    ` ' '`---^`   '`---^`---|`---'`       |\n");
+	printf("|                                                                   `---'             |\n");
+	printf("|=====================================================================================|\n");
+
+
+
+	fwrite(STDOUT, "\n\nActions:\n 1. View Contacts\n 2. Add Contact\n 3. Delete Contact\n 4. Search Contacts\n 5. Exit\n");
+	function promptUser(){
+		do{
+		fwrite(STDOUT, "Please Select an Action by Number: ");
+		$response = trim(fgets(STDOUT));
+		}while (!$response);
+		return $response;
+		echo "\r";
+	}
+		switch (promptUser()) {
+			case 1:
+				echo "You have selected Contacts\n";
+				
+				break;
+			case 2:
+				echo "You have selected Add Contact\n";
+				break;
+			case 3:
+				echo "You have selected Delete Contact\n";
+				break;
+			case 4:
+				echo "You have selected Search Contacts\n";
+				break;
+			case 5:
+				echo "You have selected Exit\n";
+				break;
+			case null:
+				useSameLine();
+				echo "Please enter a valid response!\n";
+				break;
+			default:
+				useSameLine();
+				echo "Please enter a valid response: ";
+				break;
+		}
+
+
+	fclose($handle);
